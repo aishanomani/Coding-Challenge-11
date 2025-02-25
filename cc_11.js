cc6d80d981ed80b31a355623c4354f3a4019ec92
Aisha Nomani <aishan@usf.edu>
2025-02-25 15:41:24 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -19,6 +19,26 @@ console.log(book1.getDetails()); // Expected Output: "Title: The Great Gatsby, A
 book1.updateCopies(-1);
 console.log(book1.getDetails()); // Expected Output: "Title: The Great Gatsby, Author: F. Scott Fitzgerald, ISBN: 123456, Copies: 4"
 
+// Task 2: Creating a Borrower Class
+console.log("********** Created Borrower Class **********");
+class Borrower {
+    constructor(name, borrowerId) {
+        this.name = name;
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+    borrowBook(book) {
+        this.borrowedBooks.push(book);
+    }
+    returnBook(book) {
+        this.borrowedBooks = this.borrowedBooks.filter(b => b !== book);
+    }
+}
+const borrower1 = new Borrower("Alice Johnson", 201);
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // Expected output: ["The Great Gatsby"]
+borrower1.returnBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks); // Expected output: []
 
 
 
